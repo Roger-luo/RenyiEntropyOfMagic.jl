14f972edc8c2eca15aeacd6b0e9107fc4b30df92
Roger-Luo <rogerluo.rl18@gmail.com>
2022-01-27 21:46:24 -0500

    run using

--- src/cli.jl
+++ src/cli.jl
@@ -2,12 +2,14 @@ using Comonicon
 
 @cast function scan_tfim(N::Int; nsamples::Int=10^6, nprocs::Int=0)
     nprocs > 0 && addprocs(nprocs; exeflags=["--project=$(pkgdir(RenyiEntropyOfMagic))"])
+    @everywhere using RenyiEntropyOfMagic
     results = scan_tfim_renyi_entropy(N; nsamples)
     serialize(pkgdir(RenyiEntropyOfMagic, "data", "$N-sites-result.dat"), results)
 end
 
 @cast function prepare_state(N::Int;nprocs::Int=0)
     nprocs > 0 && addprocs(nprocs; exeflags=["--project=$(pkgdir(RenyiEntropyOfMagic))"])
+    @everywhere using RenyiEntropyOfMagic
     prepare_states(N, 0.1:0.1:2.5)
 end
 
