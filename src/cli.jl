8d0f0f5d046e016d27295bff770eb3747c4a2938
Roger-Luo <rogerluo.rl18@gmail.com>
2022-01-27 21:52:08 -0500

    fix  cli

--- src/cli.jl
+++ src/cli.jl
@@ -1,15 +1,19 @@
 using Comonicon
 
-@cast function scan_tfim(N::Int; nsamples::Int=10^6, nprocs::Int=0)
-    nprocs > 0 && addprocs(nprocs; exeflags=["--project=$(pkgdir(RenyiEntropyOfMagic))"])
-    @everywhere using RenyiEntropyOfMagic
+@cast function scan_tfim(N::Int; nsamples::Int=10^6, nprocs::Int=1)
+    if nprocs > 1
+        procs = addprocs(nprocs-1; exeflags=["--project=$(pkgdir(RenyiEntropyOfMagic))"])
+        Distributed.remotecall_eval(Main, procs, Expr(:toplevel, :(using RenyiEntropyOfMagic)))
+    end
     results = scan_tfim_renyi_entropy(N; nsamples)
     serialize(pkgdir(RenyiEntropyOfMagic, "data", "$N-sites-result.dat"), results)
 end
 
-@cast function prepare_state(N::Int;nprocs::Int=0)
-    nprocs > 0 && addprocs(nprocs; exeflags=["--project=$(pkgdir(RenyiEntropyOfMagic))"])
-    @everywhere using RenyiEntropyOfMagic
+@cast function prepare_state(N::Int;nprocs::Int=1)
+    if nprocs > 1
+        procs = addprocs(nprocs-1; exeflags=["--project=$(pkgdir(RenyiEntropyOfMagic))"])
+        Distributed.remotecall_eval(Main, procs, Expr(:toplevel, :(using RenyiEntropyOfMagic)))
+    end
     prepare_states(N, 0.1:0.1:2.5)
 end
 
